55d3feb71028e2b8613a752e06e2112c64a46bf3
刘----仨 <15879806727@163.com>
2025-10-15 17:48:42 +0800

    feat: 使用TRPC context管理服务

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,13 +1,58 @@
-import { initTRPC } from "@trpc/server"
+import { initTRPC, TRPCError } from "@trpc/server"
+import { Session } from "inspector/promises"
+import { getServerSession } from "next-auth"
 
+export async function createTRPContest() {
+  const session = await getServerSession()
 
+  if (!session?.user) {
+    throw new TRPCError({
+      code: 'FORBIDDEN'
+    })
+  }
 
-const t = initTRPC.create()
+  return {
+    session
+  }
+}
+
+const t = initTRPC.context<typeof createTRPContest>().create()
 
 const { router, procedure } = t
 
+const middleware = t.middleware(async ({ ctx, next }) => {
+  const start = Date.now()
+
+  const result = await next()
+
+  console.log('---> api time: ', Date.now() - start)
+
+  return result
+})
+
+const checkLoginMiddleware = t.middleware(async ({ ctx, next }) => {
+  if (!ctx.session?.user) {
+    throw new TRPCError({
+      code: "FORBIDDEN"
+    })
+  }
+
+  return next()
+})
+
+const loggedProduce = procedure.use(middleware)
+
+const protestedProcedure = procedure.use(checkLoginMiddleware)
+
 export const testRouter = router({
-  hello: procedure.query(() => {
+  hello: loggedProduce.query(async ({ ctx }) => {
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(null)
+      }, 1000)
+    })
+
     return {
       hello: 'world'
     }
