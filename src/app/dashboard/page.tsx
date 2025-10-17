625094524ef45fc278337c77e3f1ddd356fa0819
刘----仨 <15879806727@163.com>
2025-10-17 19:14:19 +0800

    feat: 乐观UI,上传完成实时更新图片列表

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
-import { Uppy } from "@uppy/core"
+import { Uppy, UppyFile } from "@uppy/core"
 import AWSS3 from "@uppy/aws-s3"
 import { useEffect, useState } from "react";
-import { useUppyState } from "@uppy/react";
 import { trpcClientReact, trpcPureClient } from "@/utils/api"
 import { Button } from "@/components/ui/button";
 import { UploadButton } from "@/components/feature/UploadButton";
@@ -13,6 +12,8 @@ import { Dropzone } from "@/components/feature/Dropzone";
 import { cn } from "@/lib/utils";
 import { usePasteFile } from "@/hooks/usePasteFile";
 import { UploadPreview } from "@/components/feature/UploadPreview";
+import { useUppyState } from "./hooks/useUppyState";
+
 export default async function Home() { // 添加 async 关键字
 
   const [uppy] = useState<Uppy>(() => {
@@ -31,6 +32,13 @@ export default async function Home() { // 添加 async 关键字
     return uppyInstance;
   });
 
+  const utils = trpcClientReact.useUtils()
+
+  const { data: fileList, isPending } = trpcClientReact.file.listFiles.useQuery()
+
+  const [uploadingFileIDs, setUploadingFileIDs] = useState<string[]>([])
+  const uppyFiles = useUppyState(uppy, (s) => s.files)
+
   useEffect(() => {
     const handler = (file: any, resp: any) => {
       if (file) {
@@ -38,17 +46,39 @@ export default async function Home() { // 添加 async 关键字
           name: file.data instanceof File ? file.data.name : "test",
           path: resp.uploadURL ?? "",
           type: file.data.type
+        }).then((resp) => {
+          utils.file.listFiles.setData(void 0, (prev) => {
+            if (!prev) {
+              return prev
+            }
+            return [resp, ...prev]
+          })
         })
       }
     }
+
+    const uploadProgressHandler = (data: any) => {
+      if (data) {
+        setUploadingFileIDs((currentFiles) => [...currentFiles, ...data.fileIDs])
+      }
+    }
+
+    const completeHandler = () => {
+      setUploadingFileIDs([])
+    }
+
+    uppy.on('upload', uploadProgressHandler)
+
     uppy.on("upload-success", handler)
+
+    uppy.on('complete', completeHandler)
 
     return () => {
       uppy.off("upload-success", handler)
+      uppy.off('upload', uploadProgressHandler)
+      uppy.off('complete', completeHandler)
     }
   }, [uppy])
-
-  const { data: fileList, isPending } = trpcClientReact.file.listFiles.useQuery()
 
   usePasteFile({
     onFilesPaste: (files) => {
@@ -86,6 +116,30 @@ export default async function Home() { // 添加 async 关键字
                 </div>
               )}
 
+              {
+                uploadingFileIDs.length > 0 && uploadingFileIDs.map((id) => {
+                  const file = uppyFiles[id]
+
+                  const isImage = file.data.type.startsWith("image");
+
+                  const url = URL.createObjectURL(file.data)
+
+                  return (
+                    <div key={file.id} className="w-56 h-56 flex justify-center items-center border-red-500">
+                      {isImage ? (
+                        <img src={url} alt={file.name} />
+                      ) : (
+                        <Image
+                          src="/public/unknown-file-types.png"
+                          alt="unknown-file-types"
+                          width={100}
+                          height={100}
+                        />
+                      )}
+                    </div>
+                  );
+                })
+              }
               {fileList?.map((file) => {
                 const isImage = file.contentType.startsWith("image");
 
