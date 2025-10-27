35fe1934e1279b0708c48f7fd620246af5b15bdc
刘----仨 <15879806727@163.com>
2025-10-27 17:04:32 +0800

    feat: 在页面上发请求，修改cors

---
+++ examples/nuxt/app/apps/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Hello World</div>
+</template>
+
+<script setup>
+import { createApiClient } from "@image-sass/api"
+import { onMounted } from "vue";
+
+const apiClient = createApiClient({ apiKey: "asd" })
+
+onMounted(() => {
+  apiClient.file.createPresignedUrl.mutate({
+    filename: "屏幕截图 2024-08-19 173036.png",
+    contentType: "image/png",
+    size: 10378,
+    appId: "796bda6b-ff3c-4cfc-bfb4-330a0e62693d"
+  })
+})
+
+
+</script>
--- src/app/api/open/[...trpc]/route.ts
+++ src/app/api/open/[...trpc]/route.ts
@@ -1,13 +1,31 @@
 import { NextRequest } from "next/server"
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch" // 添加此导入
 import { openRouter } from "@/server/open-router"
-const handler = (request: NextRequest) => {
-  return fetchRequestHandler({
+const handler = async (request: NextRequest) => {
+  const res = await fetchRequestHandler({
     endpoint: "/api/open",
     req: request,
     router: openRouter,
     createContext: () => ({})
   })
+
+  res.headers.append("Access-Control-Allow-Origin", "*")
+  res.headers.append("Access-Control-Allow-Methods", "*")
+  res.headers.append("Access-Control-Allow-Headers", "*")
+
+  return res
+}
+
+export function OPTIONS() {
+  const res = new Response("", {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "*"
+    }
+  })
+
+  return res
 }
 
 
