3866ff8b5f149de2685b55f0bf4d592966fd47a9
刘----仨 <15879806727@163.com>
2025-10-30 15:45:39 +0800

    feat: 配置页面导航优化，增加返回按钮

--- src/app/dashboard/@nav/apps/[id]/setting/api-key/page.tsx
+++ src/app/dashboard/@nav/apps/[id]/setting/api-key/page.tsx
@@ -1,44 +1,15 @@
 'use client'
 
-import { trpcClientReact } from "@/utils/api";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { use } from "react";
-
+import { BreadCrumb } from "../breadcrumb";
 export default function AppDashboardNav({ params }: { params: Promise<{ id: string }> }) {
-  const { data: apps, isPending } = trpcClientReact.apps.listApps.useQuery()
 
   const { id } = use(params);
 
-  const currentApp = apps?.filter(app => app.id === id)[0]
 
   return (
     <div className="flex justify-between items-center">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost">
-            {
-              isPending ? 'Loading...' : currentApp ? currentApp.name : '...'
-            }
-          </Button>
-
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          {
-            apps?.map(app => {
-              return <DropdownMenuItem key={app.id} disabled={app.id === id}>
-                <Link href={`/dashboard/apps/${app.id}`}>
-                  {app.name}
-                </Link>
-              </DropdownMenuItem>
-            })
-          }
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <div>
-        {` / api Keys`}
-      </div>
+      <BreadCrumb id={id} leaf="Api Keys"></BreadCrumb>
     </div>
-  )
+  );
 }
--- src/app/dashboard/@nav/apps/[id]/setting/storage/page.tsx
+++ src/app/dashboard/@nav/apps/[id]/setting/storage/page.tsx
@@ -1,46 +1,15 @@
 'use client'
 
-import { trpcClientReact } from "@/utils/api";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { use } from "react";
-
+import { BreadCrumb } from "../breadcrumb";
 
 export default function AppDashboardNav({ params }: { params: Promise<{ id: string }> }) {
 
   const { id } = use(params);
 
-  const { data: apps, isPending } = trpcClientReact.apps.listApps.useQuery()
-
-  const currentApp = apps?.filter(app => app.id === id)[0]
-
   return (
     <div className="flex justify-between items-center">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost">
-            {
-              isPending ? 'Loading...' : currentApp ? currentApp.name : '...'
-            }
-          </Button>
-
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          {
-            apps?.map(app => {
-              return <DropdownMenuItem key={app.id} disabled={app.id === id}>
-                <Link href={`/dashboard/apps/${app.id}`}>
-                  {app.name}
-                </Link>
-              </DropdownMenuItem>
-            })
-          }
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <div>
-        {` / storage`}
-      </div>
+      <BreadCrumb id={id} leaf="Storage"></BreadCrumb>
     </div>
-  )
+  );
 }
